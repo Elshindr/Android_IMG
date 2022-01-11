05f9097567a52884b6fa18cfd74d844cd32a1044
Elshindr <headkeytics@gmail.com>
2022-01-11 13:50:12 +0100

    Ajout class AccesWS accéder au Service Soap

--- app/src/main/java/com/example/coach/controleur/Controle.java
+++ app/src/main/java/com/example/coach/controleur/Controle.java
@@ -3,75 +3,72 @@ package com.example.coach.controleur;
 import android.content.Context;
 
 import com.example.coach.modele.AccesDistant;
-import com.example.coach.modele.AccesLocal;
 import com.example.coach.modele.Profil;
-import com.example.coach.outils.Serializer;
-import com.example.coach.vue.CalculActivity;
 
 import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Controleur
+ */
 public final class Controle {
-    private static Controle instance = null;
+
+    private static Controle instance = null ;
     private static Profil profil;
-    private ArrayList<Profil> lesProfils;
-    private static String nomFic = "saveProfil";
-    private static AccesLocal accesLocal;
+    private static String nomFic = "saveprofil";
     private static AccesDistant accesDistant;
-    private static Context activity;
+    private static Context context;
+    private ArrayList<Profil> lesProfils;
 
     /**
-     * constructeur privé de Controle
+     * constructeur privé
      */
-    private Controle() {
+    private Controle(){
         super();
     }
 
     /**
-     * Classe singleton qui récupére l'instance unique de la classe Controle ou la créée
+     * récupération de l'instance unique de Controle
      * @return instance
      */
-    public static final Controle getInstance(Context activity){
-        if(Controle.instance == null) {
+    public static final Controle getInstance(Context context){
+        if(Controle.instance == null){
             Controle.instance = new Controle();
-            if(activity != null){
-                Controle.activity = activity;
+            if(context != null) {
+                Controle.context = context;
             }
-            // Appel pour serialisation
-            // recupSerialize(activity);
-
-            //Appel pour acces local
-            //accesLocal = new AccesLocal(activity);
-            //profil = accesLocal.recupDernier();
-
             accesDistant = new AccesDistant();
-            accesDistant.envoi("tous", new JSONArray());
+            accesDistant.envoi("tous", null);
         }
         return Controle.instance;
     }
 
-    
     /**
-     * Methode qui créé une nouvelle instance de la classe Profil selon les valeurs de poids, de taille, d'age et de sexe fournies
+     * Création du profil
      * @param poids
      * @param taille en cm
      * @param age
-     * @param sexe 0 femme, 1 homme
+     * @param sexe 1 pour homme, 0 pour femme
      */
-    public void creerProfil(int poids, int taille, int age, int sexe){
-        profil  = new Profil(new Date(), poids, taille, age, sexe);
-        // Appel pour serialisation
-        // Serializer.serialize(nomFic, profil, activity);
-
-        //accesLocal.ajout(profil);
-        lesProfils.add(profil);
-        accesDistant.envoi("enreg", profil.convertToJSONArray());
+    public void creerProfil(Integer poids, Integer taille, Integer age, Integer sexe){
+        Profil unProfil = new Profil(new Date(), poids, taille, age, sexe);
+        lesProfils.add(unProfil);
+        accesDistant.envoi("enreg", unProfil.convertToJSONArray());
     }
 
     /**
-     * Methode qui appelle la methode getImg de la classe Profil et récuppére la valeur de img
+     * suppression d'un profil
+     * @param profil
+     */
+    public void delProfil(Profil profil){
+        accesDistant.envoi("suppr", profil.convertToJSONArray());
+        lesProfils.remove(profil);
+    }
+
+    /**
+     * getter sur l'img du profil
      * @return img
      */
     public float getImg(){
@@ -83,7 +80,7 @@ public final class Controle {
     }
 
     /**
-     * Methode qui appelle la methode getMessage de la classe Profil et récuppére la valeur de message
+     * getter sur le message du profil
      * @return message
      */
     public String getMessage(){
@@ -95,88 +92,69 @@ public final class Controle {
     }
 
     /**
-     * Methode qui verifie que l'objet Profil n'est pas null et renvoi la valeur de taille
-     * @return Integer taille
-     */
-    public Integer getTaille(){
-        if(!(profil == null)){
-            return profil.getTaille();
-        }
-        return null;
-    }
-
-    /**
-     * Methode qui verifie que l'objet Profil n'est pas null et renvoi la valeur de poids
-     * @return Integer poids
+     * getter sur le poids du profil
+     * @return poids
      */
     public Integer getPoids(){
-        if(!(profil == null)){
+        if(profil != null){
             return profil.getPoids();
         }
         return null;
     }
 
     /**
-     * Methode qui verifie que l'objet Profil n'est pas null et renvoi la valeur de age
-     * @return Integer age
+     * getter sur la taille du profil
+     * @return taille
      */
-    public Integer getAge(){
-        if(!(profil == null)){
-            return profil.getAge();
+    public Integer getTaille(){
+        if(profil != null){
+            return profil.getTaille();
         }
         return null;
     }
 
     /**
-     * Methode qui verifie que l'objet Profil n'est pas null et renvoi la valeur de sexe
-     * @return Integer sexe
+     * getter sur l'age du profil
+     * @return age
+     */
+    public Integer getAge(){
+        if(profil != null){
+            return profil.getAge();
+        }
+        return null;
+    }
+    /**
+     * getter sur le sexe du profil
+     * @return sexe
      */
     public Integer getSexe(){
-        if(!(profil == null)){
+        if(profil != null){
             return profil.getSexe();
         }
         return null;
     }
 
     /**
-     * Getter sur lesprofils
-     * @return lesprofils
+     * setter sur le profil
+     * @param profil
+     */
+    public void setProfil(Profil profil){
+        Controle.profil = profil;
+    }
+
+    /**
+     * getter sur lesProfils
+     * @return lesProfils
      */
     public ArrayList<Profil> getLesProfils() {
         return lesProfils;
     }
 
     /**
-     * Setter sur lesProfils
+     * setter sur lesProfils
      * @param lesProfils
      */
     public void setLesProfils(ArrayList<Profil> lesProfils) {
         this.lesProfils = lesProfils;
-    }
-
-    /**
-     * Methode qui valorise l'objet profil avec le contenu récupéré de la sérialisation
-     * @param activity
-     */
-    private static void recupSerialize(Context activity){
-        profil =  (Profil) Serializer.deSerialize(nomFic, activity);
-    }
-
-    /**
-     * Setter sur profil
-     * @param profil
-     */
-    public void setProfil(Profil profil){
-        Controle.profil = profil;
-
-        // possible mais ne marche pas car contexte est de type Context qui est une classe mère de
-        //CalculActivity. Donc les méthodes de CalculActivity ne sont pas visibles à partir de la classe Context
-        //context.recupProfil()
-        //((CalculActivity)activity).recupProfil();
-    }
-
-    public void delProfil(Profil profil){
-        accesDistant.envoi("suppr", profil.convertToJSONArray());
-        lesProfils.remove(profil);
     }
 }
--- app/src/main/java/com/example/coach/modele/AccesDistant.java
+++ app/src/main/java/com/example/coach/modele/AccesDistant.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.example.coach.controleur.Controle;
 import com.example.coach.outils.AccesHTTP;
+import com.example.coach.outils.AccesWS;
 import com.example.coach.outils.AsyncResponse;
 import com.example.coach.outils.MesOutils;
 
@@ -15,76 +16,64 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class AccesDistant implements AsyncResponse {
-    private static final String SERVERADDR = "http://192.168.1.54/coach/serveurcoach.php";
+
+    private static final String NAMESPACE = "http://192.168.1.30/coach/";
+    private static final String URL = NAMESPACE + "server.php";
     private Controle controle;
 
-
     /**
-     * Constructeur de la classe AccesDistant
+     * constructeur
      */
     public AccesDistant(){
-        controle = controle.getInstance(null);
+        controle = Controle.getInstance(null);
     }
 
 
-    /**
-     * gérer le retour asynchrone du serveur.
-     * @param output texte retourne par le serveur
-     */
     @Override
     public void processFinish(String output) {
-        Log.d("serveur", "************" + output.length());
-
+        Log.d("serveur", "************" + output);
         String[] message = output.split("%");
-        Log.d("mMEEEEEEEEErde",""+ message.length);
-        Log.d("mMEEEEEEEEErde",""+ message[0]);
-        //Log.d("mMEEEEEEEEErde",""+ message[1]);
         if(message.length > 1){
-
-            if(message[0].equals("enreg")){
-                Log.d("enreg", message[1]);
-            }
-            else if(message[0].equals("tous")){
-                Log.d("tous", message[1]);
+            if (message[0].equals("enreg")){
+                Log.d("enreg", "*********** "+message[1]);
+            }else if (message[0].equals(("tous"))){
                 try {
                     JSONArray infos = new JSONArray(message[1]);
                     ArrayList<Profil> lesProfils = new ArrayList<Profil>();
-                    for(int k = 0; k < infos.length(); k++){
+                    for(int k=0;k<infos.length();k++) {
                         JSONObject info = new JSONObject(infos.get(k).toString());
-                        Date dateMesure = MesOutils.convertStringToDate(info.getString("datemesure"), "yyyy-MM-dd hh:mm:ss");
+                        Date dateMesure = MesOutils.convertStringToDate(info.getString("datemesure"),
+                                "yyyy-MM-dd hh:mm:ss");
                         Integer poids = info.getInt("poids");
                         Integer taille = info.getInt("taille");
                         Integer age = info.getInt("age");
                         Integer sexe = info.getInt("sexe");
-
                         Profil profil = new Profil(dateMesure, poids, taille, age, sexe);
                         lesProfils.add(profil);
                     }
-                    Log.d("AcessDistant", "================"+lesProfils.size());
                     controle.setLesProfils(lesProfils);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
 
+            }else if (message[0].equals("Erreur !")){
+                Log.d("erreur", "*********** "+message[1]);
             }
-            else if(message[0].equals("Erreur !")){
-                Log.d("Erreur !", message[1]);
-            }
-       }
+        }
     }
 
     /**
-     * Envoi de données vers le serveur distant
-     * @param operation
-     * @param lesDonneesJson
+     * envoi de données vers le serveur distant
+     * @param methode
+     * @param lesDonneesJSON
      */
-    public void envoi(String operation, JSONArray lesDonneesJson){
-        AccesHTTP accesDonnees = new AccesHTTP();
+    public void envoi(String methode, JSONArray lesDonneesJSON){
+        AccesWS accesDonnees = new AccesWS(URL, NAMESPACE, methode);
         accesDonnees.delegate = this;
-
-        accesDonnees.addParam("operation", operation);
-        accesDonnees.addParam("lesdonnees", lesDonneesJson.toString());
-
-        accesDonnees.execute(SERVERADDR);
+        if(lesDonneesJSON != null) {
+            accesDonnees.addParam("lesdonnees", lesDonneesJSON.toString());
+        }
+        accesDonnees.execute();
     }
+
 }
