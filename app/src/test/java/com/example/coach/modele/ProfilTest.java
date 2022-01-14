a09ae10ab67f79ef7bc69599973184d16f8375ce
Elshindr <headkeytics@gmail.com>
2022-01-14 21:05:02 +0100

    test sQ

--- app/src/test/java/com/example/coach/modele/ProfilTest.java
+++ app/src/test/java/com/example/coach/modele/ProfilTest.java
@@ -2,11 +2,13 @@ package com.example.coach.modele;
 
 import junit.framework.TestCase;
 
+import java.util.Date;
+
 public class ProfilTest extends TestCase {
     // création d’un profil : femme de 67kg, 1m65, 35 ans
-    private Profil profil = new Profil(67, 165, 35, 0);
+    private Profil profil = new Profil(new Date(),  165,  35,  0, 1);
     // résultat de l’img correspondant
-    private float img = (float)32.2 ;
+    private float img = (float)1600.0966;
     // message correspondant
     private String message = "Trop élevé" ;
 
